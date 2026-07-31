1be16da36636b3319562f23c426dcaafeed6c23b
emmanuelpaalam <jeanparaseman@gmail.com>
2026-07-31 01:16:58 +0000

    First tab of dashboard rough draft completed - descriptive analysis plots and updated UI

--- app.R
+++ app.R
@@ -14,12 +14,28 @@ ui <- fluidPage(
       tabPanel("Descriptive Data", 
                br(),
                h3("Metabolic Syndrome Indicators"),
-               p("Distributions of key clinical variables in the All of Us cohort."),
+               p("Distributions of key clinical variables in the selected All of Us cohort."),
+               br(),
                
+               h3("Demographics"),
                fluidRow(
-                 column(6, plotOutput("bp_histogram")),
-                 column(6, plotOutput("glucose_histogram"))
-               )
+                 column(6, plotOutput("dem1")),
+                 column(6, plotOutput("dem2")),
+                 column(12, plotOutput("dem3")),
+               ),
+               
+               h3("MetS Symptoms Prevalence by Subject Group"),
+               fluidRow(column(12, plotOutput("mets_prev"))),
+               
+               h3("Metabolic Count Distribution"),
+               fluidRow(column(12, plotOutput("metcount"))),
+               
+               h3("Physical/Mental Impact of MetS"),
+               fluidRow(column(12, plotOutput("impacts"))),
+               
+               h3("Behavioral Data"),
+               fluidRow(column(6, plotOutput("smoking")),
+                        column(6, plotOutput("activity"))),
       ),
       
       tabPanel("LCA Subgroups", 
@@ -27,9 +43,6 @@ ui <- fluidPage(
                h3("Latent Class Analysis"),
                p("Visualization of distinct metabolic risk subgroups and chronic disease burden."),
                
-               fluidRow(
-                 column(12, plotOutput("lca_cluster_plot"))
-               )
       )
     )
   )
@@ -37,35 +50,13 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  # Uses fake data for now
-  output$bp_histogram <- renderPlot({
-    # Generates a fake histogram for blood pressure
-    hist(rnorm(500, mean = 130, sd = 15), 
-         main = "Systolic Blood Pressure", 
-         xlab = "mmHg", col = "#a6cee3", border = "white")
-  })
-  
-  output$glucose_histogram <- renderPlot({
-    # Generates a fake histogram for fasting glucose
-    hist(rnorm(500, mean = 105, sd = 25), 
-         main = "Fasting Blood Sugar", 
-         xlab = "mg/dL", col = "#b2df8a", border = "white")
-  })
-  
-  # --- TAB 2 Output: Dummy LCA Plot ---
-  output$lca_cluster_plot <- renderPlot({
-    # Generates a fake scatter plot showing 3 'subgroups'
-    x <- rnorm(300)
-    y <- rnorm(300)
-    clusters <- sample(c("Class 1", "Class 2", "Class 3"), 300, replace = TRUE)
-    color_map <- c("Class 1" = "#e41a1c", "Class 2" = "#377eb8", "Class 3" = "#4daf4a")
-    
-    plot(x, y, 
-         main = "Metabolic Risk Subgroups (Dummy Data)",
-         xlab = "Latent Variable 1", ylab = "Latent Variable 2",
-         col = color_map[clusters],
-         pch = 19, cex = 1.5)
-    legend("topright", legend = names(color_map), fill = color_map, bty = "n")
-  })
+  output$dem1 <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/dem_birthsex.rds")})
+  output$dem2 <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/dem_raceeth.rds")})
+  output$dem3 <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/dem_age.rds")})
+  output$mets_prev <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/metsprev.rds")})
+  output$metcount <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/metcount.rds")})
+  output$impacts <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/fatiguemet.rds")})
+  output$smoking <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/smokrisk.rds")})
+  output$activity <- renderPlot({readRDS("~/repos/MetaSenseRepo/data/actlevels.rds")})
 }
 shinyApp(ui, server)
